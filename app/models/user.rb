e6c9755fbc508279c81552f52b2ff44c67798b90
tkoprowski <thibault.koprowski@gmail.com>
2013-02-03 08:54:55 -0800

    ajout controller users

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+  attr_accessible :eamil, :nom
+end
---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "users/new.html.erb" do
+  before(:each) do
+    assign(:user, stub_model(User,
+      :nom => "MyString",
+      :eamil => "MyString"
+    ).as_new_record)
+  end
+
+  it "renders new user form" do
+    render
+
+    # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
+    assert_select "form", :action => users_path, :method => "post" do
+      assert_select "input#user_nom", :name => "user[nom]"
+      assert_select "input#user_eamil", :name => "user[eamil]"
+    end
+  end
+end
